2e57854b3613fa90b7d35c644f9cb9a529505065
Sandeshsanthu <41741287+Sandeshsanthu@users.noreply.github.com>
2025-11-20 17:52:06 +0530

    changes made on config

--- config/src/main/java/com/piggymetrics/config/SecurityConfig.java
+++ config/src/main/java/com/piggymetrics/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * @author sandeshsgowdasssssssss
+ * @author sandeshsgowdassssssssssssssssssssss
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
