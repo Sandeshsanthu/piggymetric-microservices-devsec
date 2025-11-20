e45efd875f9b41974ebc5ff066fbea9cd62d3ce1
Sandeshsanthu <41741287+Sandeshsanthu@users.noreply.github.com>
2025-11-20 22:39:53 +0530

    new push

--- auth-service/src/main/java/com/piggymetrics/auth/config/OAuth2AuthorizationConfig.java
+++ auth-service/src/main/java/com/piggymetrics/auth/config/OAuth2AuthorizationConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
- * @author cdov
+ * @author sandeshs
  */
 @Configuration
 @EnableAuthorizationServer
