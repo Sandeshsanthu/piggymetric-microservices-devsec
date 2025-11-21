c38a82282a5f8ca98c451d997108c2d9b296e697
Sandeshsanthu <41741287+Sandeshsanthu@users.noreply.github.com>
2025-11-21 16:44:58 +0530

    updated action filing

--- registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
+++ registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
@@ -3,7 +3,9 @@ package com.piggymetrics.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/*
+test
+*/
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryApplication {
