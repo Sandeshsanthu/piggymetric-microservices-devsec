0310cc3f007aec8d883a5a296d14d04fc7c9153e
Sandeshsanthu <41741287+Sandeshsanthu@users.noreply.github.com>
2025-11-21 15:27:00 +0530

    updated action filesss

--- account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
+++ account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 /*
-new services
+new servicesssss
 */
 @FeignClient(name = "auth-service")
 public interface AuthServiceClient {
